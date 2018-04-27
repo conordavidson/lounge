101250ae555dc90e90befcb4791fd82bf77f976d
Conor Davidson <conor@conordavidson.com>
2018-04-27 16:28:31 -0400

    Next movie on movie end.

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -4,19 +4,25 @@ import './style.css'
 
 class Player extends Component {
   render() {
-    const { _STATUS_INITIALIZED, _STATUS_LOADING, currentMovie } = this.props
+    const {
+      _STATUS_INITIALIZED,
+      _STATUS_LOADING,
+      currentMovie,
+      actions: { nextMovie }
+    } = this.props
     return (
       <div className={`PlayerComponent`}>
         {_STATUS_INITIALIZED && !_STATUS_LOADING ? (
           <div className={`PlayerComponent__player`}>
             <YouTube
               videoId={currentMovie.trailer.key}
+              onEnd={nextMovie}
               opts={{
                 width: '100%',
                 height: '100%',
                 playerVars: {
                   autoplay: 1,
-                  controls: 0,
+                  controls: 1,
                   modestbranding: 1,
                   rel: 0,
                   showinfo: 0,
--- src/containers/PlayerContainer.js
+++ src/containers/PlayerContainer.js
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import Player from 'components/Player'
 import currentMovie from 'state/selectors/currentMovie'
+import { nextMovie } from 'state/actions'
 
 const mapStateToProps = state => {
   return {
@@ -9,4 +11,15 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(Player)
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators(
+      {
+        nextMovie
+      },
+      dispatch
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player)
