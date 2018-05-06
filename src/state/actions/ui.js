4cfd82e20c151b4e1504e533eceab4ff6a0f9530
Conor Davidson <conor@conordavidson.com>
2018-05-05 23:56:36 -0400

    Refactor controler/mouse hider to use sagas.

--- src/state/actions/ui.js
+++ src/state/actions/ui.js
@@ -1,28 +1,9 @@
-let controlDisplayTimer
-
 export const START_CONTROL_DISPLAY_TIMEOUT = 'START_CONTROL_DISPLAY_TIMEOUT'
-export const startControlDisplayTimeout = () => dispatch => {
-  clearTimeout(controlDisplayTimer)
-  controlDisplayTimer = setTimeout(
-    () => dispatch(endControlDisplayTimeout()),
-    4000
-  )
-  return dispatch({
-    type: START_CONTROL_DISPLAY_TIMEOUT
-  })
+export const startControlDisplayTimeout = () => {
+  return { type: START_CONTROL_DISPLAY_TIMEOUT }
 }
-
 export const END_CONTROL_DISPLAY_TIMEOUT = 'END_CONTROL_DISPLAY_TIMEOUT'
-export const endControlDisplayTimeout = () => {
-  return {
-    type: END_CONTROL_DISPLAY_TIMEOUT
-  }
-}
-
 export const CANCEL_CONTROL_DISPLAY_TIMEOUT = 'CANCEL_CONTROL_DISPLAY_TIMEOUT'
-export const cancelControlDisplayTimeout = () => dispatch => {
-  clearTimeout(controlDisplayTimer)
-  return dispatch({
-    type: CANCEL_CONTROL_DISPLAY_TIMEOUT
-  })
+export const cancelControlDisplayTimeout = () => {
+  return { type: CANCEL_CONTROL_DISPLAY_TIMEOUT }
 }
