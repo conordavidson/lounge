66aa7fae0deb9a25e2b6ce9e7fbef2bae9267026
Conor Davidson <conor@conordavidson.com>
2018-05-06 20:57:51 -0400

    Add local storage persistance.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,6 +7,10 @@ import root from 'state/sagas'
 import createSagaMiddleware from 'redux-saga'
 import thunk from 'redux-thunk'
 
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
+
 import List from 'containers/ListContainer'
 import Player from 'containers/PlayerContainer'
 import Query from 'containers/QueryContainer'
@@ -17,37 +21,47 @@ import './style.css'
 import 'styles/reset.css'
 import 'styles/transitions.css'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['list']
+}
+const persistedReducers = persistReducer(persistConfig, reducers)
+
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
-  reducers,
+  persistedReducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   compose(applyMiddleware(
     thunk,
     sagaMiddleware
   ))
 )
+const persistor = persistStore(store)
 sagaMiddleware.run(root)
 
 export default () => (
   <Provider store={store}>
-    <main>
-      <Player />
-      <div className={`Top`}>
-        <div className={`Top__left`}>
-          <List />
-        </div>
-        <div className={`Top__right`}>
-          <div className={`Top__info`}>
-            <Info />
+    <PersistGate loading={null} persistor={persistor}>
+      <main>
+        <Player />
+        <div className={`Top`}>
+          <div className={`Top__left`}>
+            <List />
           </div>
-          <div className={`Top__query`}>
-            <Query />
+          <div className={`Top__right`}>
+            <div className={`Top__info`}>
+              <Info />
+            </div>
+            <div className={`Top__query`}>
+              <Query />
+            </div>
           </div>
         </div>
-      </div>
-      <div className={`Bottom`}>
-        <Controls />
-      </div>
-    </main>
+        <div className={`Bottom`}>
+          <Controls />
+        </div>
+      </main>
+    </PersistGate>
   </Provider>
 )
